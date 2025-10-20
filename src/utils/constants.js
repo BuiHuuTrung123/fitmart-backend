adb8f67aefe7bdf2f5ae837ce3fc69548a445fc2
BuiHuuTrung123 <trungbuimanifest@gmail.com>
2025-10-20 20:19:57 +0700

    deploy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,8 @@
 import { env } from '~/config/environment'
 export const WHITELIST_DOMAINS = [
    'http://localhost:5173',
-    'https://trello.trungbui.dev'
+    'https://trello.trungbui.dev',
+   'http://lacumart-frontend.vercel.app',
 ]
 // export const BOARD_TYPES = {
 //    PRIVATE: 'private',
